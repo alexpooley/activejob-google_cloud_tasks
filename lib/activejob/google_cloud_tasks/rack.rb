2a111359732af969933eda0065afc1eebbfc1d25
Alex Pooley <alex@brownbeagle.com.au>
2019-10-03 09:10:31 +0800

    rack endpoint will perform where ever it is mounted

--- lib/activejob/google_cloud_tasks/rack.rb
+++ lib/activejob/google_cloud_tasks/rack.rb
@@ -1,32 +1,31 @@
 require 'rack'
 
 # Rack handler endpoint to perform the task.
-# Expects a `PATH_INFO` of /perform with a `QUERY_STRING` that
-# contains a `job` and `params` key. Where `job` is the name
-# of the handler class and `params` is a serialized string of
-# arguments for the handler.
+#
+# Expects a `QUERY_STRING` that contains a `job` and `params` key.
+#
+# Where:
+# - `job` is the name of the handler class
+# - `params` is a serialized string of arguments for the handler
+#
 module Activejob
   module GoogleCloudTasks
     class Rack
       class << self
         def call(env)
-          if env['PATH_INFO'].match(/^\/perform/)
-            params = parsed_params(env['QUERY_STRING'])
-            unless params.has_key?(:job)
-              raise StandardError, "Job is not specified."
-            end
-
-            klass(params[:job]).perform_now(*params[:params])
-            [200, {}, ['ok']]
-          else
-            [404, {}, ['not found']]
+          params = parsed_params(env['QUERY_STRING'])
+          unless params.has_key?(:job)
+            raise StandardError, "Job is not specified."
           end
+
+          klass(params[:job]).perform_now(*params[:params])
+          [200, {}, ['ok']]
         end
 
         private
 
         def klass(job)
-          Kernel.const_get(job.camelize)
+          Kernel.const_get(job)
         end
 
         def parsed_params(query_string)
--- spec/activejob/rack_spec.rb
+++ spec/activejob/rack_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Activejob::GoogleCloudTasks::Rack do
     expect(app).to receive(:klass) { job_klass }
 
     params = { job: 'GreetJob', params: ["foo", ":)", {:prefix=>"howdy"}] }
-    get '/perform', params
+    get '/any-mounted-path', params
     expect(last_response.status).to eq(200)
     expect(job_klass).to have_received(:perform_now).with(*params[:params])
   end
 
   it 'raises NameError for unknown job' do
-    expect { get '/perform', job: 'UnknownJob' }.to raise_error(NameError)
+    expect { get '/any-mounted-path', job: 'UnknownJob' }.to raise_error(NameError)
   end
 end
